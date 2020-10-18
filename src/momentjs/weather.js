579fc3397bc477a80863e8e5f0fdf5935999d9a8
c11g <renimo@hanmail.net>
2020-10-18 20:42:38 +0900

    remove geo coords storage save

--- src/momentjs/weather.js
+++ src/momentjs/weather.js
@@ -1,35 +1,26 @@
 const weather = document.querySelector('.js-weather');
 
 const WEHATHER_API_KEY = '9e84af8f0c78d3f82698d7750262af94';
-const COORDS_LS = 'coords';
 
 async function getWeather(lat, long){
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${WEHATHER_API_KEY}&units=metric`;
-  // fetch(url)
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     const temp = data.main.temp;
-  //     const location = data.name;
-  //     printWeather(temp, location);
-  //   });
-  const res = await fetch(url);
-  const data = await res.json();
-  const temp = data.main.temp;
-  const location = data.name;
-  printWeather(temp, location);
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    const temp = data.main.temp;
+    const location = data.name;
+    printWeather(temp, location);
+  } catch (e) {
+    weather.innerText = e
+  }
 }
 
 function printWeather(temp, loc){
   weather.innerText = `${temp}° at ${loc}`;
 }
 
-function saveCoords(coords){
-  localStorage.setItem(COORDS_LS, JSON.stringify(coords));
-}
-
 function handleGeoSucces(position){
   const {latitude, longitude} = position.coords;
-  saveCoords({latitude, longitude})
   getWeather(latitude, longitude);
 }
 
@@ -41,17 +32,7 @@ function askForCoords(){
   navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
 }
 
-function loadCoords(){
-  const loadedCoords = localStorage.getItem(COORDS_LS);
-  if (!loadedCoords) {
-    askForCoords();
-  } else {
-    const {latitude, longitude} = JSON.parse(loadedCoords);
-    getWeather(latitude, longitude);
-  }
-}
-
 function weatherInit(){
-  loadCoords();
+  askForCoords();
 }
 weatherInit();
