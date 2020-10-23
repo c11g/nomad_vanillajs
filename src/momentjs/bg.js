0b640136e502fdd128b627cb9db76132b63698bc
choyoungkwang <choyoungkwang@nts-corp.com>
2020-10-23 12:21:02 +0900

    add default bg image, sort button

--- src/momentjs/bg.js
+++ src/momentjs/bg.js
@@ -1,9 +1,14 @@
 const wrap = document.querySelector('.wrap');
 
 async function getRandomPhoto(){
-  const res = await fetch(`https://api.unsplash.com/photos/random?client_id=${config.UNSPLASH_API_KEY}`);
-  const data = await res.json();
-  const imgUrl = data.urls.regular;
+  let imgUrl = "./courtney-smith-h3X5J-5orT8-unsplash.jpg"
+  try {
+    const res = await fetch(`https://api.unsplash.com/photos/random?client_id=${config.UNSPLASH_API_KEY}`);
+    const data = await res.json();
+    imgUrl = data.urls.regular;
+  } catch(error) {
+    console.log(error)
+  }
   wrap.style.backgroundImage = `url(${imgUrl})`;
 }
 
